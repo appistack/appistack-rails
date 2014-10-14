42cc5a280fea9aa6c8e727ee597828cb3354e643
David Conner <dconner.pro@gmail.com>
2014-10-13 23:52:42 -0400

    * adding basic test suite

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe UsersController do
+  describe 'As a user' do
+    describe '#index' do
+
+    end
+
+    describe '#create' do
+
+    end
+
+    describe '#show' do
+
+    end
+  end
+
+  describe 'As a guest' do
+
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,15 @@
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+require 'minitest/pride'
+require 'mocha/mini_test'
+
+ActiveRecord::Migration.maintain_test_schema!
+
+require 'database_cleaner'
+DatabaseCleaner.strategy = :transaction
+
+require 'rails/test_help'
+class ActiveSupport::TestCase
+  before { DatabaseCleaner.start }
+  after { DatabaseCleaner.clean }
+end
