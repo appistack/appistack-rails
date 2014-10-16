dd987981fe873b30a44e161c1aea273167011771
David Conner <dconner.pro@gmail.com>
2014-10-16 01:45:37 -0400

    * removed redirect url

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,5 @@ Rails.application.routes.draw do
     end
   end
 
-  get '/*url', to: redirect(Rails42::Application.config.client_app_url)
   get '/', to: redirect(Rails42::Application.config.client_app_url)
 end
