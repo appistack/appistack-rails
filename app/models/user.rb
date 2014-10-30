a83d31bdee011dceadef40127dcd1e78de3d1c2c
David Conner <dconner.pro@gmail.com>
2014-10-30 06:26:19 -0400

    * added a user's roles to the view model.
    * not implementing view models at this point to remain generic

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,13 @@ class User < ActiveRecord::Base
   # TODO: login with email or username
   validates :email, format: /@/
   validates :username, length: { in: 4..20, allow_blank: true }
+
+  def as_json(opts = nil)
+    # doing it this way because i don't want implementation-specific view models
+    super.merge(roles: global_roles.map {|r| r.name})
+  end
+
+  def global_roles
+    self.roles.where(resource_type: nil)
+  end
 end
