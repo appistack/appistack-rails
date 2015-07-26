af9fb335fea5720961bddb2dc1344b40af77186a
David Conner <dconner.pro@gmail.com>
2015-07-26 17:38:08 -0400

    ensuring all requests are json

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,15 @@ class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
   include Pundit
   protect_from_forgery with: :null_session
-
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_filter :ensure_json_request
 
   protected
+
+  def ensure_json_request
+    return if params[:format] == "json" || request.headers["Accept"] =~ /json/
+    render :nothing => true, :status => 406
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :username
