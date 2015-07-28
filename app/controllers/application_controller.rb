7cc4096e0436de17e03e63b1b4ec6afd427ccffc
David Conner <dconner.pro@gmail.com>
2015-07-28 16:31:11 -0400

    accidentally disabled email confirmation routes by enforcing json requests across the application
    * woopsies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,8 @@ class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery with: :null_session
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_filter :ensure_json_request
 
   protected
-
-  def ensure_json_request
-    return if params[:format] == "json" || request.headers["Accept"] =~ /json/
-    render :nothing => true, :status => 406
-  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :username
--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -1,3 +1,11 @@
 class BaseController < ApplicationController
   before_action :authenticate_user!
+  before_filter :ensure_json_request
+
+  protected
+
+  def ensure_json_request
+    return if params[:format] == "json" || request.headers["Accept"] =~ /json/
+    render :nothing => true, :status => 406
+  end
 end
