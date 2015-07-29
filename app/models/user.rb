27813810ce748b88287080d75fe97b1cfa1b6440
David Conner <dconner.pro@gmail.com>
2015-07-29 02:26:36 -0400

    * added a more strongly worded warning to the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,9 @@ class User < ActiveRecord::Base
   def as_json(opts = nil)
     # doing it this way because i don't want implementation-specific view models
     # NOTE: do not modify this method.  remove it and use something for view models.
-    #   changes to as_json will change Devise's responses
+    #   changes to as_json will change Devise's responses, possibly exposing secure user data
+    #   data such as email/uid will be included when user model is nested within other models
+    # .... so change it!
     super.merge(gravatar_url: gravatar_url, roles: global_roles.map {|r| {name: r.name} })
   end
 
